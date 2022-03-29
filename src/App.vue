b3f5f0384a0105fa4dd4e4832f8eb329a1d2bf90
Hasan Yahya <hasanyah@gmail.com>
2022-03-29 15:41:15 +0200

    Send a basic POST req after reading the local turtle template

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
+<script setup>
+import axios from 'axios'
+import sampleRdf from "./assets/sample.rdf?raw";
+computed: {
+  console: () => console
+}
+function parserMco() {                   
+  // console.log(sampleRdf)
+  const headers = {
+    'Content-Type' : 'plain/text'
+  }
+  axios.post("https://scm.linkeddata.es/api/parser/mco", "text", {headers})
+  .then(response => Console.log(response))
+  .catch(error => {
+    this.errorMessage = error.message;
+    console.error("There was an error!", error);
+  });
+}
+</script>
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
@@ -5,5 +25,6 @@
     <div class="wrapper">
       Hello World in Vue
     </div>
+    <button @click="parserMco()">Send MCO Post Request</button>
   </header>
 </template>
